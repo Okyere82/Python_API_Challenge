76af624f45c8c1124aa9494f0e7448e72cf5f131
Okyere Gyebi <okyeregyebi123@gmail.com>
2021-05-17 04:44:26 -0400

    Uploaded New file

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "97a762fc51f146e70453777622372ffd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD6X1ScC38f0q7Cm--fDBEE8kH2hctHkL0"
